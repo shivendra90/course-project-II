388eb2563be41f729b4490a44b853fa8d373a527
Shivendra Sharma <shivendra.sharma090@yahoo.com>
2015-09-27 23:33:59 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,13 +1,17 @@
 library(dplyr)
 library(data.table)
 
+#Load datasets
 NEI <- readRDS('summarySCC_PM25.rds')
 SCC <- readRDS('Source_Classification_Code.rds')
 
+#Subset for Baltimore city
 baltimore = NEI[NEI$fips == '24510']
 
+#Get the summations by year for Baltimore
 sumations.baltimore = baltimore[,.(Emissions = sum(Emissions)), by = year]
 
+#Plot graphics
 png('plot2.png', 800,600)
 plot.space <- c(0,4)
 plot(sumations.baltimore$year, summations.baltimore$Emissions, type = 'l', xlab = 'Year', 
@@ -15,3 +19,4 @@ plot(sumations.baltimore$year, summations.baltimore$Emissions, type = 'l', xlab 
 lines(summations.baltimore$Emissions, col = 'red')
 
 dev.off()
+#End
